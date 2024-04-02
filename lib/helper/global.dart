76313d6fc5dd00f5e280b7f0afebdf9ae7c7d03c
Shahriar Islam Sazid <70763173+shahriar00@users.noreply.github.com>
2024-04-03 00:02:41 +0600

    global.dart

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -1,4 +1,1 @@
-//String apikey = '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';
-//String apikey = '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';
-
- String apikey = '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';
+String apikey = '';
